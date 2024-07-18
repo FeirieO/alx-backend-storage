eacc980f3d4e08bc7e51a75981109d4141254063
FeirieO <feirie001@gmail.com>
2024-07-18 22:35:17 +0100

    minor fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,20 +3,26 @@
     obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
-r = redis.Redis()
-count = 0
 
+r = redis.Redis()
 
 def get_page(url: str) -> str:
     """ track how many times a particular URL was accessed in the key
         "count:{url}"
         and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
-    resp = requests.get(url)
+    
+    # Increment the access count
     r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    
+    cached_content = r.get(f"cached:{url}")
+    
+    if cached_content:
+        return cached_content.decode('utf-8')
+    
+    resp = requests.get(url)
+    r.setex(f"cached:{url}", 10, resp.text)
+    
     return resp.text
 
-
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    print(get_page('http://slowwly.robertomurray.co.uk'))
